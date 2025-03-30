cd3f99c9c8a90d41fed81072cb2338f273badafa
Charles Chang <80582567+2n-1@users.noreply.github.com>
2025-03-30 18:41:22 +0800

    Change data fetch method

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
 import csv
 import requests
+import base64
+import json
 
 # API網址，用static.py產生的檔案
-url = "https://raw.githubusercontent.com/yzucse-cc/ops-mid/refs/heads/main/static.json"
+url = "https://api.github.com/repos/yzucse-cc/ops-mid/contents/static.json"
 
 # 拿資料
 response = requests.get(url)
@@ -11,15 +13,26 @@ response.encoding = response.apparent_encoding
 # 轉json
 data = response.json()
 
-if data:    # 如果有資料
-    header = data[0].keys() # 取出標題列
+if response.status_code == 200:
+    data = response.json()  # 轉json
+    base64_content = data.get("content", "").strip()  # 取得 content
 
-    # 寫入csv
-    with open("api.csv", "w", newline="", encoding="utf-8") as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=header)
-        writer.writeheader()
-        writer.writerows(data)
-    print("json->csv done")
+    if base64_content:
+        decoded_content = base64.b64decode(base64_content).decode("utf-8")  # 解碼 Base64
+        data = json.loads(decoded_content)  # 轉json
+
+    if data:    # 如果有資料
+        header = data[0].keys() # 取出標題列
+
+        # 寫入csv
+        with open("api.csv", "w", newline="", encoding="utf-8") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=header)
+            writer.writeheader()
+            writer.writerows(data)
+        print("json->csv done")
+
+    else:
+        print("no data in json")
 
 else:
-    print("no data in json")
+    print("no json found")
